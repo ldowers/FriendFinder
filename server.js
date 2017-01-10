77b483e5337a77ec1485ac9e850b23407cd26e75
Lori Dowers <ldowers@cfl.rr.com>
2017-01-10 10:55:56 -0500

    Updates to server.js and friends.js

---
+++ server.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Express Configuration
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Router
+// =============================================================
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+// Listener
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
